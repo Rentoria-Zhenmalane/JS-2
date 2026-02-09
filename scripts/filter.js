db1c82d09bef708685a93c4cfa61e5f7d7229116
Shane <zhenren1098@gmail.com>
2026-02-09 17:23:49 +0800

    .map() and .filter()
    Done learning the fundamentals of map() through activities. Added filter().js to learn fundamentals.

---
+++ scripts/filter.js
@@ -0,0 +1,99 @@
+
+
+//ACTIVITY1
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNum = numbers.filter(num => num % 2 === 0)
+console.log(evenNum);
+
+
+//Acitivity 2: Filter the array so that it only includes words with 3 letters or less.
+
+const words = ["hi", "hello", "hey", "bye", "good"];
+const newWord = words.filter(function (word) {
+  /*
+    if (word.length <= 3) {
+      return word; //returns the string if condition is true 
+    }
+      //works but code below is cleaner and semantically correct.
+  */
+  return word.length <= 3; //cleaner version
+  // Returns the boolean itself
+});
+console.log(newWord);
+
+
+
+//ACTIVITY 3 (medium): Return only students who passed (score ≥ 75).
+
+const students = [
+  { name: "Anna", score: 80 },
+  { name: "Ben", score: 70 },
+  { name: "Cara", score: 90 }
+]
+
+const passed = students.filter((student) => {
+  return student.score >= 75;
+});
+console.log(passed);
+
+
+//ACTIVITY 4 (medium): Filter out strings that don’t include “@”.
+const emails = [
+  "user@gmail.com",
+  "invalidEmail",
+  "admin@yahoo.com",
+  "hello.world"
+];
+
+const newE = emails.filter((e) => {
+  if (e.includes("@")) { // .includes() is a JS method that checks if a string has a certain value.
+    return e;
+  };
+});
+console.log(newE);
+
+
+//ACTIVITY 5 (HARD): Return users who are active AND at least 18 years old.
+
+const users = [
+  { name: "Leo", age: 18, active: true },
+  { name: "Mia", age: 22, active: false },
+  { name: "Noah", age: 19, active: true }
+];
+
+const u = users.filter(function (user) {
+  /*
+  if (user.age >= 18 && user.active === true) {
+    return user;
+  };
+  //Works but the code below is much clenaer and semantically correct.
+  */
+  return user.age >= 18 && user.active //active is already a boolean so no need to active === true
+
+});
+console.log(u);
+
+
+//ACTIVITY 6 (HARD): Filter products that are in stock and cost less than 500.
+
+const products = [
+  { name: "Shoes", price: 1200, inStock: true },
+  { name: "Shirt", price: 450, inStock: true },
+  { name: "Cap", price: 300, inStock: false },
+];
+
+const p = products.filter(function (product) {
+  if (product.price <= 500 && product.inStock === true) {
+    return product;
+  };
+});
+console.log(p);
+
+/*
+filter() asks one question for each item:
+❓ Should this item stay in the array?
+👉 Answer with true or false
+That’s it. Nothing more.
+
+*/
--- scripts/map.js
+++ scripts/map.js
@@ -1,12 +1,126 @@
 /*
+
+//ACTIVITY 1
 const numbers = [1, 2, 3, 4];
 const doubledNumbers = numbers.map(num => num * 2
 )// doesn't need to use return. Will return automatically
 
 console.log(doubledNumbers);
 
+console.log('Result: ', transformed);
+
+
+//ACTIVITY 3
+const names = ["anna", "john", "mike"];
+
+const newNames = names.map((name) => {
+  return name.toUpperCase()
+}) //in JavaScript when using a curly braces You need to explicitly use return, otherwise values will return undefined
+console.log(newNames);
+
+
+
+//ACTIVITY 5
+const prices = [100, 250, 400];
+const tax = 0.12; //This will be our thisValue in map()
+
+const totalTax = prices.map(function (price) {
+  return price + (price * tax) //Used PEMDAS here for shorter syntax
+}, tax)// passed the value of tax from above(0.12) so we can use it later for computation.
+
+console.log('VAT: ', totalTax);
+
+//ACTIVITY 6
+//Use map() to return a new array where: Each name is uppercase, Each name is prefixed with "@"
+
+const users = ["zhen", "alex", "maria"];
+
+const newNames = users.map((name) => {
+  return "@" + name.toUpperCase();
+});
+
+console.log(newNames);
+
+
+//ACTIVITY 7
+//Use map() to return a new array where: Each number is multiplied by itself
+
+const numbers = [2, 4, 6, 8];
+
+const squaredNumber = numbers.map(number => number * number);
+console.log(squaredNumber);
+
+
+//ACTIVITY 8 
+//Use map() to return a new array of objects like this:
+
+//Rules:
+//score >= 90 → "A"
+//score >= 80 → "B"
+//score >= 70 → "C"
+//score >= 60 → "D"
+//below 60 → "F"
+
+const scores = [95, 82, 67, 58, 74];
+
+const result = scores.map(function (score) {
+  let grade
+
+  if (score >= 90) {
+    grade = 'A'
+  } else if (score >= 80) {
+    grade = 'B'
+  } else if (score >= 70) {
+    grade = 'C'
+  } else if (score >= 60) {
+    grade = 'D'
+  } else {
+    grade = 'F'
+  }
+
+  return {grade, score }; //returns the values in an object array 
+
+});
+console.log(result);
+
+
+
+const items = [
+  {
+    name: 'shirt',
+    price: 500
+  },
+  {
+    name: 'pants',
+    price: 1200
+  },
+  {
+    name: 'shoes',
+    price: 2500
+  }
+];
+
+
+
+const newItem = items.map((item) => {
+  let discountedPrice
+
+  discountedPrice = item.price - 0.2 * item.price
+  return {
+    name: item.name,
+    price: item.price,
+    discountedPrice: discountedPrice
+  };
+})
+
+console.log(newItem);
+*/
+
+
+
+//ACTIVITY 2
 //BASIC SYNTAX FOR map()
-const numbers = [1, 2, 3]; 
+const numbers = [1, 2, 3];
 const multiplier = 10;
 
 const transformed = numbers.map(function (currentValue, i, arr) {
@@ -18,16 +132,26 @@ const transformed = numbers.map(function (currentValue, i, arr) {
 }, multiplier); // We’ll use "thisValue" to provide a multiplier 
 
 
-console.log('Result: ', transformed);
+/*
+Map works only on:
+    ->real arrays
+    ->things that behave like arrays if borrowed
+
+-You can’t skip items with map().
+-map() is an array method that ALWAYS takes a function and ALWAYS returns a new array by transforming each element.
+-map() transforms, mutation methods modify.
+-map() is just a fancy loop that returns a new array.
+
+map() = loop → transform → return → new array
+
+Mutation methods:
+push()
+pop()
+splice()
+sort()
+reverse()
+
 */
-
-const names = ["anna", "john", "mike"];
-
-const newNames = names.map((name) => {
-  return name.toUpperCase()
-}) //in JavaScript when using a curly braces You need to explicitly use return, otherwise values will return undefined
-console.log(newNames);
-
 
 
 
